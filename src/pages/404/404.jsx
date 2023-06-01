67150e986eae0771187aa5a208695ecd759a1de8
Mohsin raza <iammohsinamjad@gmail.com>
2023-06-01 15:32:09 +0500

    movix app code completed

---
+++ src/pages/404/404.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+
+
+const PageNotFound = () => {
+    return (
+        <div>
+            page not found
+        </div>
+    )
+};
+
+
+
+export default PageNotFound;
